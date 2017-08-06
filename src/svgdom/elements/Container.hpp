ad3c12200dedcd35b8302b13c88e46eb45a14073
JaimeIvanCervantes <jimmycc80@hotmail.com>
2017-08-06 13:58:32 -0700

    Container: Added pushBackUnique() function to simplify appending elements with unique ptrs

--- src/svgdom/elements/Container.hpp
+++ src/svgdom/elements/Container.hpp
@@ -13,10 +13,18 @@ struct Container{
 	std::vector<std::unique_ptr<Element>> children;
 
 	/**
+	 * @brief Push backs element with unique pointer.
+	 * @param e - Element to push back (with unique pointer).
+	 */
+	template <class E>
+	void pushBackUnique(E e) { children.push_back(utki::makeUnique<E>(e)); }
+
+	/**
 	 * @brief Relay accept to children.
 	 * @param visitor - visitor to accept.
 	 */
 	void relayAccept(svgdom::Visitor& visitor)const;
+
 };
 
 }
